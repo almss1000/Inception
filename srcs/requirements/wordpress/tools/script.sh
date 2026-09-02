ce22b9049fae57aec7097f24c4b2f3bb641fd858
almss1000 <alamine10095@gmail.com>
2026-09-02 05:06:20 -0400

    ok for second user for wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -34,6 +34,14 @@ if [ ! -f "$WP_PATH/wp-config.php" ]; then
         --admin_password="$WP_ADMIN_PASSWORD" \
         --admin_email="$WP_ADMIN_EMAIL"
 
+    wp user create \
+        --allow-root \
+        --path="$WP_PATH" \
+        "$WP_USER" \
+        "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role="editor"
+
 fi
 
 chown -R www-data:www-data "$WP_PATH"
